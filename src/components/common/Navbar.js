27debbfb27ceebf971347d627fc8daca0792bd7f
Ashirwada <ashirwada93@gmail.com>
2022-11-18 09:53:59 +0530

    Adding theme to navbar and footer

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,8 +1,10 @@
+import '../../css/common.css';
+
 export const Navbar = ({ username }) => {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary m-2">
+    <nav className="navbar navbar-expand-sm nav-bar-custom m-2">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" style={{color:'#d7e2de', fontSize:'25px'}} href="/">
           <img
             src="favicon.svg"
             alt="Logo"
@@ -15,25 +17,25 @@ export const Navbar = ({ username }) => {
         <div id="section-left" className="collapse navbar-collapse">
           <ul className="navbar-nav fs-5">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/">
+              <a className="nav-link active" style={{color:'#d7e2de', fontSize:'18px'}} aria-current="page" href="/Home">
                 Overview
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link active" href="#">
+              <a className="nav-link" style={{color:'#d7e2de', fontSize:'18px'}} href="/Accounts">
                 Accounts
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" style={{color:'#d7e2de', fontSize:'18px'}} href="/Offers">
                 Offers
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="font-theme-light nav-link" style={{color:'#d7e2de', fontSize:'18px'}} href="/Investments">
                 Investments
               </a>
             </li>
@@ -43,16 +45,16 @@ export const Navbar = ({ username }) => {
         <div id="section-right">
           <ul className="navbar-nav fs-3">
             <li className="nav-item mx-1">
-              <a className="nav-link bi bi-gear" href="#">
+              <a className="nav-link bi bi-gear" style={{color:'#d7e2de'}} href="#">
               </a>
             </li>
 
             <li className="nav-item mx-1">
-              <a className="nav-link bi bi-envelope" href="#"></a>
+              <a className="nav-link bi bi-envelope" style={{color:'#d7e2de'}} href="#"></a>
             </li>
 
             <li className="nav-item mx-1">
-              <a className="nav-link bi bi-bell" href="#"></a>
+              <a className="nav-link bi bi-bell" style={{color:'#d7e2de'}} href="#"></a>
             </li>
 
             <li className="nav-item dropdown-center mx-1">
@@ -62,13 +64,14 @@ export const Navbar = ({ username }) => {
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
+                style={{color:'#d7e2de'}}
               >
-                <i className="bi bi-person-circle"></i>
+                <i className="bi bi-person-circle" style={{color:'#d7e2de'}}></i>
               </a>
               
               <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="#">Logout</a>
+                  <a className="dropdown-item" style={{color:'#d7e2de'}} href="#">Logout</a>
                 </li>
               </ul>
             </li>
