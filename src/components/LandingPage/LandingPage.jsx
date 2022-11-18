a3a24f5b104c9821f6e7d4eade202865dd9fbcb2
Ashirwada <ashirwada93@gmail.com>
2022-11-18 09:56:10 +0530

    Adding landing page and  components

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../../css/LandingPage.css'
+import {SliderView} from "./Slider/SliderView";
+import Row from "react-bootstrap/Row";
+import {MainAccDetailView} from "./MainAccDetail/MainAccDetailView";
+import {AccountListView} from "./AccountList/AccountListView";
+import {TransactionListView} from "./TransactionList/TransactionListView"
+import {ExpenseView} from "./ExpenseView/ExpenseView"
+import Container from "react-bootstrap/Col";
+
+export const LandingPage = () => {
+
+  return(
+    <Container style={{display:'grid'}}>
+      <div className = "home-container">
+        <div>
+          <div className="float-child">
+            <SliderView />
+          </div>
+          <div className="float-child">
+            <MainAccDetailView />
+          </div>
+        </div>
+        <Row><AccountListView /></Row>
+        <div>
+          <div className='float-child'>
+            <TransactionListView />
+          </div>
+          <div className='float-child'>
+            <ExpenseView />
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
