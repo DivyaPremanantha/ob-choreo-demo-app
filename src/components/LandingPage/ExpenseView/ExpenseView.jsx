fff1ff38bceab88da5eb229920928f7808e39091
Divya Premanantha <premananthadivya@gmail.com>
2022-11-23 01:46:57 +0530

    Modify expense view

--- src/components/LandingPage/ExpenseView/ExpenseView.jsx
+++ src/components/LandingPage/ExpenseView/ExpenseView.jsx
@@ -7,15 +7,77 @@ import Col from "react-bootstrap/Col";
 import { ListGroup } from "react-bootstrap";
 import { useState, useEffect } from 'react';
 import { CONSTANTS } from '../../../services/utils';
+import { getTransactions } from '../../../services/transaction-service';
 
 var transactionInfo
 var daily = '$123.40'
 var weekly = '$423.20'
 var monthly = '$1237.01'
-var accessToken = sessionStorage.getItem(CONSTANTS.user_access_token)
+var user_access_token = sessionStorage.getItem(CONSTANTS.user_access_token)
 
 export const ExpenseView = () => {
   const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    if (user_access_token && transactionInfo == null) {
+      getTransactions(user_access_token).then(data => {
+        transactionInfo = data.data.Data
+        setResponse(transactionInfo)
+        daily = '$154.67'
+        weekly = '$561.83'
+        monthly = '$1797.00'
+
+        transactionInfo.Transaction.map(transaction => {
+          if (transaction.TransactionReference === "Food") {
+            ExpenseData[0].amount = parseInt(transaction.Amount.Amount) + ExpenseData[0].amount
+          } else if (transaction.TransactionReference === "Health") {
+            ExpenseData[1].amount = parseInt(transaction.Amount.Amount) + ExpenseData[1].amount
+          } else if (transaction.TransactionReference === "Clothing") {
+            ExpenseData[2].amount = parseInt(transaction.Amount.Amount) + ExpenseData[2].amount
+          } else if (transaction.TransactionReference === "Bills") {
+            ExpenseData[3].amount = parseInt(transaction.Amount.Amount) + ExpenseData[3].amount
+          } else {
+            ExpenseData[4].amount = parseInt(transaction.Amount.Amount) + ExpenseData[4].amount
+          }
+        })
+      })
+    }
+  }, [transactionInfo])
+
+  var transactionHeaderConstant = loadTransactionsHeaderView(transactionInfo);
+  var transactionDataConstant = loadTransactionsView(transactionInfo);
+
+  return (
+    <Container className="transaction-list-container">
+      <h5>All Expenses</h5>
+      {transactionHeaderConstant}
+      <div className="row-padding font-size-small font-color-orange">Last Month</div>
+      {transactionDataConstant}
+    </Container>
+  )
+}
+
+const loadTransactionsHeaderView = (transactionInfo) => {
+  return (
+    <Row>
+      <Col>
+        <div className="font-size-small font-color-orange">Daily</div>
+        <div className="font-size-small font-color-dark">{daily}</div>
+      </Col>
+      <Col>
+        <div className="font-size-small font-color-orange">Weekly</div>
+        <div className="font-size-small font-color-dark">{weekly}</div>
+      </Col>
+      <Col>
+        <div className="font-size-small font-color-orange">Monthly</div>
+        <div className="font-size-small font-color-dark">{monthly}</div>
+      </Col>
+    </Row>
+  )
+}
+
+const loadTransactionsView = (transactionInfo) => {
+
   const colorScheme = [
     '#ffc808',
     '#ff7300',
@@ -23,90 +85,34 @@ export const ExpenseView = () => {
     '#E79363',
     '#EEE10D',
     '#D9B991'
-  ]
-
-  useEffect(() => {
-    fetch("https://c112eada-316e-46a7-9705-df75e4a30edc-prod.e1-us-east-azure.choreoapis.dev/azyh/openbankingapi/1.0.0/transactions?backendServiceClientID=AtyObUX7_MyCLy6rHiGyny6Hqc0a&backendServiceClientSecret=77_F_mg63Lge9O8ujWptWui8X18a", {
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      }
-    })
-      .then(res => res.json())
-      .then(
-        (data) => {
-          setResponse(data)
-          if (accessToken) {
-            transactionInfo = data.Data
-            daily = '$154.67'
-            weekly = '$561.83'
-            monthly = '$1797.00'
-
-            transactionInfo.Transaction.map(transaction => {
-              if (transaction.TransactionReference == "Food") {
-                ExpenseData[0].amount = parseInt(transaction.Amount.Amount) + ExpenseData[0].amount
-              } else if (transaction.TransactionReference == "Health") {
-                ExpenseData[1].amount = parseInt(transaction.Amount.Amount) + ExpenseData[1].amount
-              } else if (transaction.TransactionReference == "Clothing") {
-                ExpenseData[2].amount = parseInt(transaction.Amount.Amount) + ExpenseData[2].amount
-              } else if (transaction.TransactionReference == "Bills") {
-                ExpenseData[3].amount = parseInt(transaction.Amount.Amount) + ExpenseData[3].amount
-              } else {
-                ExpenseData[4].amount = parseInt(transaction.Amount.Amount) + ExpenseData[4].amount
-              }
-            })
-          }
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-  }, [])
-
-  if (transactionInfo || accessToken == null) {
+  ];
+  if (transactionInfo || user_access_token == null) {
     return (
-      <Container className="transaction-list-container">
-        <h5>All Expenses</h5>
-        <Row>
-          <Col>
-            <div className="font-size-small font-color-orange">Daily</div>
-            <div className="font-size-small font-color-dark">{daily}</div>
-          </Col>
-          <Col>
-            <div className="font-size-small font-color-orange">Weekly</div>
-            <div className="font-size-small font-color-dark">{weekly}</div>
-          </Col>
-          <Col>
-            <div className="font-size-small font-color-orange">Monthly</div>
-            <div className="font-size-small font-color-dark">{monthly}</div>
-          </Col>
-        </Row>
-        <div className="row-padding font-size-small font-color-orange">Last Month</div>
-        <div className="pie-chart-view">
-          <Col xs={8}>
-            <PieChart width={150} height={150}>
-              <Pie data={ExpenseData} dataKey="amount" outerRadius={75}>
-                {
-                  ExpenseData.map((entry, index) => <Cell key={index} fill={colorScheme[index % colorScheme.length]} />)
-                }
-              </Pie>
-            </PieChart>
-          </Col>
-          <Col>
-            <ListGroup>
+      <div className="pie-chart-view">
+        <Col xs={8}>
+          <PieChart width={150} height={150}>
+            <Pie data={ExpenseData} dataKey="amount" outerRadius={75}>
               {
-                ExpenseData.map((entry, index) =>
-                  <ListGroup.Item key={index} className="expense-list-view font-size-small font-color-dark">
-                    <i className="bi bi-square-fill expense-list-icon"
-                      style={{ color: colorScheme[index % colorScheme.length] }}>
-                    </i>
-                    {entry.name}
-                  </ListGroup.Item>
-                )
+                ExpenseData.map((entry, index) => <Cell key={index} fill={colorScheme[index % colorScheme.length]} />)
               }
-            </ListGroup>
-          </Col>
-        </div>
-      </Container>
-    );
+            </Pie>
+          </PieChart>
+        </Col>
+        <Col>
+          <ListGroup>
+            {
+              ExpenseData.map((entry, index) =>
+                <ListGroup.Item key={index} className="expense-list-view font-size-small font-color-dark">
+                  <i className="bi bi-square-fill expense-list-icon"
+                    style={{ color: colorScheme[index % colorScheme.length] }}>
+                  </i>
+                  {entry.name}
+                </ListGroup.Item>
+              )
+            }
+          </ListGroup>
+        </Col>
+      </div>
+    )
   }
 }
