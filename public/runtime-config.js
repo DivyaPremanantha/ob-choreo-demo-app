f2ef3a419b48a4155aa5b765e95fdc577d01d5d5
Divya Premanantha <premananthadivya@gmail.com>
2023-02-23 22:29:35 +0530

    Modify choreo token endpoint

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,5 +1,5 @@
 window.env = {
-    CHOREO_SERVER_URL: "https://sts.choreo.dev",
+    CHOREO_SERVER_URL: "https://api.asgardeo.io/t/choreotestorganization",
     CHOREO_API_HOST: "https://c112eada-316e-46a7-9705-df75e4a30edc-prod.e1-us-east-azure.choreoapis.dev/azyh",
 
     CHOREO_APP_CONSUMER_KEY: "m35lKTGxoRrCtuLuG4GzNNS9vlga",
