d762c150ceb1e2c713c53270716245da2ecdcdac
Divya Premanantha <premananthadivya@gmail.com>
2023-02-24 02:23:35 +0530

    Update APP credentials

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -2,8 +2,8 @@ window.env = {
     CHOREO_SERVER_URL: "https://api.asgardeo.io/t/choreotestorganization",
     CHOREO_API_HOST: "https://c112eada-316e-46a7-9705-df75e4a30edc-prod.e1-us-east-azure.choreoapis.dev/azyh",
 
-    CHOREO_APP_CONSUMER_KEY: "m35lKTGxoRrCtuLuG4GzNNS9vlga",
-    CHOREO_APP_CONSUMER_SECRET: "GfsqaJ6rDfTN50l1HUR22iiK0Nga",
+    CHOREO_APP_CONSUMER_KEY: "9iqESSVfI3RlsXraOJZjqy0uEska",
+    CHOREO_APP_CONSUMER_SECRET: "U_OVRjdnZhPxebwjNffYfnHtn1Ya",
 
     HOST_URL: "https://purple-bay-005990a00.2.azurestaticapps.net"
 };
